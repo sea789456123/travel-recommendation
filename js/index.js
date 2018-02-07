004b792846ce06fdc215addb11520bb8ec552837
Seah <blueseaangel@likelion.org>
2018-02-07 07:25:36 +0000

    +hyeonwoo

---
+++ js/index.js
@@ -0,0 +1,33 @@
+/*
+
+
+// Picture from Happy D
+// http://drbl.in/kLRD
+
+Thanks for all the comments, suggestions <3
+
+PayPal contributions appreciated: paypal.me/ruandre
+
+
+Helpful gist for copy and paste here:
+  https://gist.github.com/ruandre/9f6f9671a20e4007b743c866f29192eb
+
+
+Notes:
+* Most logos are from https://iconmonstr.com but I tweaked them
+* Compressed via https://petercollingridge.appspot.com/svg-editor
+* Used https://gist.github.com/ruandre/7b47cbf2a4c55dac9adb#file-fittoartboard-jsx
+* IE8 and lower gets a text version
+
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+https://opensource.org/licenses/MIT
+
+*/
